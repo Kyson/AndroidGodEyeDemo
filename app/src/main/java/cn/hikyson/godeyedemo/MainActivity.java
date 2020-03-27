a20ca836ceea72203ee8c2a27fe79517c44f4db4
hui.zhao <hui.zhao@Ctrip.com>
2020-03-27 13:34:18 +0800

    upgrade to 3.4.1 support multi process support debug and release

--- app/src/main/java/cn/hikyson/godeyedemo/MainActivity.java
+++ app/src/main/java/cn/hikyson/godeyedemo/MainActivity.java
@@ -12,17 +12,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.List;
 
+import cn.hikyson.godeye.core.GodEye;
 import cn.hikyson.godeye.core.GodEyeHelper;
 import cn.hikyson.godeye.core.exceptions.UninstallException;
+import cn.hikyson.godeye.core.internal.modules.crash.CrashInfo;
+import cn.hikyson.godeye.core.internal.modules.network.NetworkInfo;
+import cn.hikyson.godeye.core.internal.modules.sm.BlockInfo;
 import cn.hikyson.godeye.core.internal.modules.startup.StartupInfo;
-import cn.hikyson.godeye.monitor.GodEyeMonitor;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import xcrash.XCrash;
 
 public class MainActivity extends Activity {
+    CompositeDisposable mCompositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +55,13 @@ public class MainActivity extends Activity {
                 });
             }
         });
+        observeWhenRelease();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        cleanupWhenRlease();
     }
 
     public void request(View view) {
@@ -102,19 +112,76 @@ public class MainActivity extends Activity {
     }
 
     private static String getNote() {
-        String openAddress = "AndroidGodEye dashboard is available on [http://localhost:5390/index.html], use plugin to open it.";
-        String condition = "Install Android Studio plugin(Named 'AndroidGodEye') [https://plugins.jetbrains.com/plugin/12114-androidgodeye] to view details.";
-        String logcat = "You can find the address in logcat by search 'AndroidGodEye monitor is running at port'.";
-        return openAddress + "\n\n" + condition + "\n\n" + logcat;
-    }
-
-    private void openBrowser(String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url));
-        startActivity(intent);
+        if ("debug".equalsIgnoreCase(BuildConfig.BUILD_TYPE)) {
+            String note1 = "This is a debug apk. Debug monitor is opened. Notification config is opened.";
+            String openAddress = "AndroidGodEye dashboard is available on [http://localhost:5390/index.html], use plugin to open it.";
+            String condition = "Install Android Studio plugin(Named 'AndroidGodEye') [https://plugins.jetbrains.com/plugin/12114-androidgodeye] to view details.";
+            String logcat = "You can find the address in logcat by search 'AndroidGodEye monitor is running at port'.";
+            return openAddress + "\n\n" + condition + "\n\n" + logcat;
+        } else {
+            String note1 = "This is a release apk. Debug monitor is closed. Notification config is closed.";
+            return note1;
+        }
     }
 
     public void makeCrash(View view) {
         XCrash.testJavaCrash(true);
+    }
+
+    private void observeWhenRelease() {
+        if (!"debug".equalsIgnoreCase(BuildConfig.BUILD_TYPE)) {
+            mCompositeDisposable = new CompositeDisposable();
+            try {
+                mCompositeDisposable.add(GodEye.instance().observeModule(GodEye.ModuleName.NETWORK, new Consumer<NetworkInfo>() {
+                    @Override
+                    public void accept(NetworkInfo networkInfo) throws Exception {
+                        AndroidSchedulers.mainThread().scheduleDirect(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "This is NetworkInfo message from release:" + networkInfo.summary, Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                }));
+            } catch (UninstallException e) {
+                e.printStackTrace();
+            }
+            try {
+                mCompositeDisposable.add(GodEye.instance().observeModule(GodEye.ModuleName.SM, new Consumer<BlockInfo>() {
+                    @Override
+                    public void accept(BlockInfo blockInfo) throws Exception {
+                        AndroidSchedulers.mainThread().scheduleDirect(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "This is BlockInfo message from release:" + blockInfo.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                }));
+            } catch (UninstallException e) {
+                e.printStackTrace();
+            }
+            try {
+                mCompositeDisposable.add(GodEye.instance().observeModule(GodEye.ModuleName.CRASH, new Consumer<List<CrashInfo>>() {
+                    @Override
+                    public void accept(List<CrashInfo> crashInfos) throws Exception {
+                        AndroidSchedulers.mainThread().scheduleDirect(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "This is CrashInfo message from release:" + crashInfos.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                }));
+            } catch (UninstallException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void cleanupWhenRlease() {
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.dispose();
+        }
     }
 }
